b50b2f0f7a9b7e192900f6cfa63f84917db54b04
Ravindu77 <101692013@student.swin.edu.au>
2021-10-25 02:48:09 +1100

    Update CircuitV2.ino

--- Arduino/CircuitV2/CircuitV2.ino
+++ Arduino/CircuitV2/CircuitV2.ino
@@ -8,6 +8,8 @@ int buttonState;
 int addState, minusState;
 bool pushB;
 
+bool ac, heater;
+
 const int DHT_SENSOR_PIN = 8;
 DHT_nonblocking dht_sensor( DHT_SENSOR_PIN, DHT_SENSOR_TYPE );
 
@@ -19,6 +21,9 @@ void setup() {
   buttonState = 0;
   addState = 0;
   minusState = 0;
+
+  ac = false;
+  heater = false;
   
   idealTemp = 25;
   pushB = false;
@@ -36,18 +41,79 @@ void loop() {
   buttonState = 0;
   addState = 0;
   minusState = 0;
-  
-  pushButton(buttonState, buttonPin);
-  tempChange();
 
+  readTempHumid();
+  pushButton(buttonState, buttonPin);
+  
+  tempChange();
   if(pushB){
     LedOn(YLed);
-    readTempHumid();
+    tempControl();
   }else{
     allOff();
     blinkY();
   }
   
+}
+
+void tempControl(){
+  //delay(1000);
+  if(( idealTemp < temperature)){
+    heaterOn();
+  }
+  if( idealTemp > temperature){
+    acOn();
+  }
+  if(idealTemp == temperature){
+    allOff();
+  }
+}
+
+void acOn(){
+  if(heater){
+    heaterOff();
+  }
+  if(ac){
+    LedOn(GLed);
+  }else{
+    ac = true;
+    LedOn(GLed);
+  }
+}
+
+void acOff(){
+  if(ac){
+    ac = false;
+    LedOff(GLed);
+  }else{
+    LedOff(GLed);
+  }
+}
+
+void heaterOn(){
+  if(ac){
+    acOff();
+  }
+  if(heater){
+    LedOn(RLed);
+  }else{
+    heater = true;
+    LedOn(RLed);
+  }
+}
+
+void heaterOff(){
+  if(heater){
+    heater = false;
+    LedOff(RLed);
+  }else{
+    LedOff(RLed);
+  }
+}
+
+void allOff(){
+  acOff();
+  heaterOff();
 }
 
 
@@ -73,11 +139,6 @@ void blinkY(){
   delay(300);
   LedOff(YLed);
   delay(300);
-}
-
-void allOff(){
-  LedOff(RLed);
-  LedOff(GLed);
 }
 
 void pushButton(int state, int pin){
@@ -115,11 +176,11 @@ void LedOff(int led){
 void readTempHumid(){
   //dht_sensor.measure(&temperature, &humidity);
   if(dht_sensor.measure(&temperature, &humidity)){
-    Serial.print( idealTemp, 1 );
-    Serial.print( " " );
+    Serial.print( "Temp: " );
     Serial.print( temperature, 1 );
     Serial.print( " " );
+    Serial.print( "Humid: " );
     Serial.print( humidity, 1 );
-    Serial.println();
+    Serial.println("%");
   }
 }
