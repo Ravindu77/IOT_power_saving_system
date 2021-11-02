7d0aa7cc13d31203430dd97717c53838534439b8
Ravindu77 <101692013@student.swin.edu.au>
2021-11-03 04:45:43 +1100

    Update piConnect.py

--- Raspberry_Pi/piConnect.py
+++ Raspberry_Pi/piConnect.py
@@ -109,11 +109,11 @@ def splitDecodeInput(decodeInput, date, client):
         if check_float(temp):
             jsonStr = tempChangeToJson(temp, date)
             #client.publish("readings-topic", jsonStr, 1)
-            client.publish("readings-topic", jsonStr, 1)
+            client.publish("power-topic", jsonStr, 1)
         else:
             jsonStr = powerToJson(temp, date)
-            #client.publish("readings-topic", jsonStr, 1)
-            client.publish("readings-topic", jsonStr, 1)
+            #client.publish("readings-topic", jsonStcr, 1)
+            client.publish("tempChange-topic", jsonStr, 1)
     
 #json translation
 def readingsToJson(splitInput, date):
@@ -185,7 +185,7 @@ def cliHandler(input_queue):
 #End Thread
 
 
-q = Queue()#Todo coud be use for future developments
+q = Queue()
 t1 = Thread(target=serialListner, args=(q, "Client01"))
 t2 = Thread(target=mqttListner, args=(q, "Client02", "publish-topic"))
 t3 = Thread(target=internalQueueReader, args=(q, "Client03"))
